09799eab7bceab0cd2df8ff3f0492e3d67e16c12
Agustin <esparta.01.af@gmail.com>
2024-02-23 01:43:17 -0300

    implementacion de la ruta para obtener un Diver por su ID

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const allDrivers = require("../controllers/allDrivers");
+const getDriverById = require("../controllers/getDriverById")
 
 const router = Router();
 
 router.get("/drivers", allDrivers);
+router.get("/drivers/:idDriver", getDriverById);
 
 module.exports = router;
