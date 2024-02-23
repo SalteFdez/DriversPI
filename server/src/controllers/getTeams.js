dc0cb4f3b152483bcafedcb587988d2f7e0fc867
Agustin <esparta.01.af@gmail.com>
2024-02-23 16:34:11 -0300

    Resolucion de bugs a la hora de traer las escuderias

--- server/src/controllers/getTeams.js
+++ server/src/controllers/getTeams.js
@@ -13,11 +13,16 @@ const getTeams = async (req, res) => {
           // Si la base de datos está vacía, obtener equipos de la API
             const response = await axios.get('http://localhost:5000/drivers');
             const apiDrivers = response.data;
-            console.log(apiDrivers)
+            //console.log(apiDrivers)
           // Extraer y procesar la lista de equipos de los drivers de la API
             const teamsFromApi = apiDrivers.reduce((teamsList, driver) => {
+        // Asegurarse de que driver.teams es una cadena antes de dividir
+            if (typeof driver.teams === 'string' || driver.teams instanceof String) {
             const driverTeams = driver.teams.split(',').map(team => team.trim());
             return [...teamsList, ...driverTeams];
+          } else {
+            return teamsList;
+          }
             }, []);
         
           // Eliminar duplicados y guardar los equipos en la base de datos
